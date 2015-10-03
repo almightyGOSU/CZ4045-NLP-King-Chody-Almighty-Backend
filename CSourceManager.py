49ed9576f671002a82a7d784b02f9d257ef3da53
Bryden <bcho002@e.ntu.edu.sg>
2015-10-03 15:41:18 +0800

    Stream Sources

--- CSourceManager.py
+++ CSourceManager.py
@@ -33,4 +33,4 @@ def getSource(pIntId):
 def getFullSource():
 
 	for intCount in range(1, CCorpusManager.getDocumentsCount()):
-		yield str(intCount) + "</br>" + CFileManager.readFromFile(str(intCount))
+		yield CFileManager.readFromFile(str(intCount))
