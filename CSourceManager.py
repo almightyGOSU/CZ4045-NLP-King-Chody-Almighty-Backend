71a542f399aa6cc7a094c1593f99c0ec8528c763
Bryden <bcho002@e.ntu.edu.sg>
2015-10-31 13:52:23 +0800

    Corpus Stats

--- CSourceManager.py
+++ CSourceManager.py
@@ -50,21 +50,7 @@ def getSourcePOS(pIntId):
 	return objSource.getPOSTags()
 
 def getSourceConcordance(pStrWord):
-	lstTokens = CDALSource.getTypes()     
-   
-   ## text = Text(lstTokens)
-   ## c = ConcordanceIndex(text.tokens, key = lambda s: s.lower())
- 
-   ## concordance_txt = ([text.tokens[map(lambda x: x-5 if (x-left_margin)>0 else 0,[offset])[0]:offset+right_margin]
-   ###                     for offset in c.offsets(pStrWord)])
-                         
-    return lstTokens
+	return CDALSource.getTypes()
 
 def getSourceSimilarity(pStrWord):
-
-	#idx = ContextIndex([word.lower( ) for word in CDALSource.getTypes()])
-	#save = []
-
-    ##save.append(idx.similar_words(pStrWord))
-
 	return CDALSource.getTypes()
